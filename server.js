dc660d225024fb998eb53bbf93738bdbace44031
stephensander1161 <57675208+stephensander1161@users.noreply.github.com>
2021-03-23 16:56:38 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
 
-var corsOptions = {
-	origin: 'http://localhost:8081'
-};
+const app = express();
+
+app.use(cors({ origin: '*' }));
 
 global.__basedir = __dirname;
-
-app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
@@ -22,6 +17,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const dotenv = require('dotenv');
 dotenv.config();
 
+const db = require('./app/models');
+db.sequelize.sync().then(() => {
+	console.log('Re-sync db.');
+});
+
 // simple route
 app.get('/', (req, res) => {
 	res.json({ message: 'Hello from server.' });
@@ -31,24 +31,6 @@ require('./app/routes/bitcoin.routes')(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-http.listen(PORT, () => {
+app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}.`);
-});
-
-io.on('connection', (socket) => {
-	console.log('Client connected');
-	dataUpdate(socket);
-});
-
-function dataUpdate(socket) {
-	socket.emit('dataupdate', Array.from({ length: 8 }, () => Math.floor(Math.random() * 40)));
-
-	setTimeout(() => {
-		dataUpdate(socket);
-	}, 2000);
-}
-
-const db = require('./app/models');
-db.sequelize.sync().then(() => {
-	console.log('Re-sync db.');
 });
